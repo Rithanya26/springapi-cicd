e3da149c27fb22b91b09b3dfa1d324f641200e3b
Rithanya26 <rithanyasubbu26@gmail.com>
2026-01-09 11:33:03 +0530

    Add HelloController to return greeting message

---
+++ src/main/java/com/example/springapi/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Java 21 CI/CD Working";
+    }
+}
